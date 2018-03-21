814060fefe4ca01c64568a523c8bceaba8cc8251
harrykarwasra <hkarwasra.97@gmail.com>
2018-03-21 13:29:51 +0530

    Removed shiity images

--- src/Denoise_CS_Grayscale_Canny.py
+++ src/Denoise_CS_Grayscale_Canny.py
@@ -4,6 +4,7 @@ from PIL import Image
 from matplotlib import pyplot as plt
 
 
+
 # Method to process the red band of the image
 def normalizeRed(intensity):
     iI      = intensity
@@ -35,15 +36,17 @@ def normalizeBlue(intensity):
     iO      = (iI-minI)*(((maxO-minO)/(maxI-minI))+minO)
     return iO
 
-path_orig = 'C:\\Users\\harry\\Documents\\Github\\StrawCounting\\Images\\'
-path_mdfd = 'C:\\Users\\harry\\Documents\\Github\\StrawCounting\\Images\\Modified\\' 
+path_orig = 'C:\\Users\\harry\\Documents\\Github\\StrawCounting\\Images\\Orig\\'
+path_mdfd = 'C:\\Users\\harry\\Documents\\Github\\StrawCounting\\Images\\Modified\\'
 path_l = []
-for i in range(44):
+
+
+for i in range(30):
     path_l.append(str(i+1))
 print (path_l)
 
-for i in range(44):
-    print(i)
+
+for i in range(30):
     img = cv2.imread(path_orig + path_l[i] + '.png')
 
     dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
@@ -51,9 +54,9 @@ for i in range(44):
 
     #plt.subplot(121),plt.imshow(img)
     #plt.subplot(122),plt.imshow(dst)
-    cv2.imwrite(path_mdfd + path_l[i] + '.png',dst)
+    cv2.imwrite(path_mdfd + path_l[i] + "_Denoise" + '.png',dst)
 
-    imageObject = Image.open(path_mdfd + path_l[i] + '.png')
+    imageObject = Image.open(path_mdfd + path_l[i] + "_Denoise" + '.png')
     #imageObject.save('C:\\Users\\harry\\Desktop\\Hackathon\\Images\\StrawHead3.png')
 
     # Split the red, green andyblue bands from the Image
@@ -61,11 +64,12 @@ for i in range(44):
     multiBands      = imageObject.split()
 
 
+
     # Apply point operations that does contrast stretching on each color band
     normalizedRedBand      = multiBands[0].point(normalizeRed)
     normalizedGreenBand    = multiBands[1].point(normalizeGreen)
     normalizedBlueBand     = multiBands[2].point(normalizeBlue)
-     
+
 
     # Create a new image from the contrast stretched red, green and blue brands
     normalizedImage = Image.merge("RGB", (normalizedRedBand, normalizedGreenBand, normalizedBlueBand))
@@ -73,7 +77,7 @@ for i in range(44):
 
     # Display the image before contrast stretching
     #imageObject.show()
-     
+
 
     # Display the image after contrast stretching
     #normalizedImage.show()
@@ -83,16 +87,12 @@ for i in range(44):
     normalizedImage1 = normalizedImage.convert("L")
     #normalizedImage1.show()
     normalizedImage1.save(path_mdfd + path_l[i] + '_Denoise_CS_GrayScale.png')
-
     img = cv2.imread(path_mdfd + path_l[i] + '_Denoise_CS_GrayScale.png',0)
     edges = cv2.Canny(img,200,100)
 
-    ##plt.s ubplot(121),plt.imshow(img,cmap = 'gray')
+    ##plt.subplot(121),plt.imshow(img,cmap = 'gray')
     ##plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     ##plt.subplot(122),plt.imshow(edges,cmap = 'gray')
     ##plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-
     cv2.imwrite(path_mdfd + 'Canny\\' + path_l[i] + '_Denoise_CS_GrayScale_Canny.png', edges)
-
-
